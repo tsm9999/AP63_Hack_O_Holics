efce57193a991a44adec2dd8d0621aee84a02d87
tsm9999 <maddiwartanush@gmail.com>
2020-07-31 23:05:49 +0530

    Initial Commit

---
+++ app/src/main/java/co/sih/presso/MainActivity.java
@@ -0,0 +1,14 @@
+package co.sih.presso;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
