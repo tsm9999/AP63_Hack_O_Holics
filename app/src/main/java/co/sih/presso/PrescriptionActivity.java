b41abecd5c623c6f23ff37ad35a12dd87d8d830e
tsm9999 <maddiwartanush@gmail.com>
2020-08-03 03:42:39 +0530

    Bottom Sheet + Recycler View

--- app/src/main/java/co/sih/presso/PrescriptionActivity.java
+++ app/src/main/java/co/sih/presso/PrescriptionActivity.java
@@ -1,14 +1,25 @@
 package co.sih.presso;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class PrescriptionActivity extends AppCompatActivity {
 
@@ -16,38 +27,14 @@ public class PrescriptionActivity extends AppCompatActivity {
     private BottomSheetBehavior sheetBehavior;
     private LinearLayout bottom_sheet;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import co.sih.presso.MedicineAdapter;
-import co.sih.presso.R;
-import co.sih.presso.SymptomAdapter;
-
-public class PrescriptionActivity extends AppCompatActivity {
-String TAG="PrescriptionActivity";
+    String TAG = "PrescriptionActivity";
     String jsonString = null;
-    String medicines=null;
-    RecyclerView rv_med,rv_sym;
-    ArrayList<String> Medicines=new ArrayList<>();
-    ArrayList<String> Symptoms=new ArrayList<>();
+    String medicines = null;
+    RecyclerView rv_med, rv_sym;
+    ArrayList<String> Medicines = new ArrayList<>();
+    ArrayList<String> Symptoms = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,19 +48,19 @@ String TAG="PrescriptionActivity";
         sheetBehavior.setPeekHeight(0);
 
 
-        openBottomSheet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
-                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                    openBottomSheet.setText("Close sheet");
-                } else {
-                    sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                    openBottomSheet.setText("Expand sheet");
-
-                }
-            }
-        });
+//            openBottomSheet.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+//                        sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+//                        openBottomSheet.setText("Close sheet");
+//                    } else {
+//                        sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+//                        openBottomSheet.setText("Expand sheet");
+//
+//                    }
+//                }
+//            });
 
         // callback for do something
         sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
@@ -104,42 +91,45 @@ String TAG="PrescriptionActivity";
 
             }
         });
-    }
-}
+
+
         Intent in = getIntent();
         jsonString = in.getStringExtra("jsonString");
 
 
         try {
-            JSONObject jsonObject=new JSONObject(jsonString);
-            medicines=jsonObject.getString("Medicines");
+            JSONObject jsonObject = new JSONObject(jsonString);
+            medicines = jsonObject.getString("Medicines");
 
-            String med[]=medicines.split("]|,|\\[");
-            for(int i=0;i<med.length;i++)
-            {
-                Log.e(TAG, "onCreate: "+med[i] );
+            String med[] = medicines.split("]|,|\\[");
+            for (int i = 0; i < med.length; i++) {
+                Log.e(TAG, "onCreate: " + med[i]);
             }
 
-        } catch (JSONException e) {
+        } catch (
+                JSONException e) {
             e.printStackTrace();
         }
 //-----------------------MEDICINES-----------------------------------------------------------------
-        rv_med = (RecyclerView) findViewById(R.id.rv_med);
+        rv_med = (RecyclerView)
+
+                findViewById(R.id.rv_med);
         rv_med.setHasFixedSize(true);
         //recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        rv_med.setLayoutManager(new LinearLayoutManager(this));
+        rv_med.setLayoutManager(new
 
-        Medicines=new ArrayList<>();
+                LinearLayoutManager(this));
+
+        Medicines = new ArrayList<>();
         Medicines.add("med1");
         Medicines.add("med2");
         Medicines.add("med3");
         Medicines.add("med4");
-        final ArrayList<String> medList= new ArrayList<>();
+        final ArrayList<String> medList = new ArrayList<>();
         Log.e("length", String.valueOf(Medicines.size()));
-        for(int i=0;i<Medicines.size();i++)
-        {
-            if(Medicines.get(i)==null)
-            {
+        for (
+                int i = 0; i < Medicines.size(); i++) {
+            if (Medicines.get(i) == null) {
                 break;
             }
             medList.add(Medicines.get(i));
@@ -153,7 +143,7 @@ String TAG="PrescriptionActivity";
         adapter.setOnItemClickListener(new MedicineAdapter.onItemClickListener() {
             @Override
             public void onItemClick(int position) {
-                String temp=medList.get(position);
+                String temp = medList.get(position);
                 //clicked(temp);
                 Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
                 adapter.notifyItemChanged(position);
@@ -161,42 +151,58 @@ String TAG="PrescriptionActivity";
         });
 
 //-------------------------------------------SYMPTOMS------------------------------------------------
-    rv_sym = (RecyclerView) findViewById(R.id.rv_sym);
-        rv_sym.setHasFixedSize(true);
-    //recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        rv_sym.setLayoutManager(new LinearLayoutManager(this));
+        rv_sym = (RecyclerView)
 
-    Symptoms=new ArrayList<>();
+                findViewById(R.id.rv_sym);
+        rv_sym.setHasFixedSize(true);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        rv_sym.setLayoutManager(new
+
+                LinearLayoutManager(this));
+
+        Symptoms = new ArrayList<>();
         Symptoms.add("sym1");
         Symptoms.add("sym2");
         Symptoms.add("sym3");
         Symptoms.add("sym4");
-    final ArrayList<String> symList= new ArrayList<>();
-        Log.e("length", String.valueOf(Medicines.size()));
-        for(int i=0;i<Symptoms.size();i++)
-    {
-        if(Symptoms.get(i)==null)
-        {
-            break;
+        final ArrayList<String> symList = new ArrayList<>();
+        Log.e("length", String.valueOf(Symptoms.size()));
+        for (
+                int i = 0; i < Symptoms.size(); i++) {
+            if (Symptoms.get(i) == null) {
+                break;
+            }
+            symList.add(Symptoms.get(i));
         }
-        symList.add(Symptoms.get(i));
-    }
 
-    //creating recyclerview adapter
-    final SymptomAdapter adapter2 = new SymptomAdapter(this, medList);
+        //creating recyclerview adapter
+        final SymptomAdapter adapter2 = new SymptomAdapter(this, symList);
 
-    //setting adapter to recyclerview
-        rv_sym.setAdapter(adapter);
+        //setting adapter to recyclerview
+        rv_sym.setAdapter(adapter2);
         adapter2.setOnItemClickListener(new SymptomAdapter.onItemClickListener() {
-        @Override
-        public void onItemClick(int position) {
-            String temp=symList.get(position);
-            //clicked(temp);
-            Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
-            adapter2.notifyItemChanged(position);
-        }
-    });
+            @Override
+            public void onItemClick(int position) {
+                String temp = symList.get(position);
+                //clicked(temp);
+                Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
+                adapter2.notifyItemChanged(position);
+
+                if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+//                        openBottomSheet.setText("Close sheet");
+                } else {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+//                        openBottomSheet.setText("Expand sheet");
+
+                }
+            }
+        });
     }
 }
+
+
+
+
 
 
