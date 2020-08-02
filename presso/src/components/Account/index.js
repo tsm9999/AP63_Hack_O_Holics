27b4f6c591bfbfe5437cc205f9fb20cda2d1ce35
Aditi Badhe <36376304+aditibadhe@users.noreply.github.com>
2020-08-02 15:09:25 +0530

    Added account settings folder

---
+++ presso/src/components/Account/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+import { AuthUserContext, withAuthorization } from '../Session';
+
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div id="aform">
+        <h3>  Account : {authUser.email}</h3>
+       <br/>
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
+);
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(AccountPage);
